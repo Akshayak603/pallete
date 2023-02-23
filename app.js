7dcf33a95e50062525c7294ef9adbd9c01e42b80
Akshayak603 <akshaykumar.ak603@gmail.com>
2023-02-23 19:01:22 +0530

    port change

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/',(req,res)=>{
     res.render('index')
 })
 
-const port=process.env.PORT || 7000;
+const port=process.env.PORT || 8000;
 app.listen(port,()=>{
     console.log(`listening on ${port}`)
 })
